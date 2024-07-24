64879cf28888c8ad6734871ddf6e7b462ead698d
Tyler <26290074+thegitduck@users.noreply.github.com>
2024-07-24 01:24:15 -0700

    add a period to test CI

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <footer className="p-6 lg:fixed lg:bottom-0 lg:left-0">
       <div>
-        visit{' '}
+        visit{" "}
         <a
           href="https://waku.gg/"
           target="_blank"
@@ -10,8 +10,8 @@ export const Footer = () => {
           className="mt-4 inline-block underline"
         >
           waku.gg
-        </a>{' '}
-        to learn more
+        </a>{" "}
+        to learn more.
       </div>
     </footer>
   );
